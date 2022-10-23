876900dd2930227f32eaa30fbef2bbd49704a758
Sukhdeepkaur11 <113074268+Sukhdeepkaur11@users.noreply.github.com>
2022-10-22 23:14:43 -0500

    first commit in univ web

---
+++ login1.js
@@ -0,0 +1,7 @@
+const loginbtn=document.getElementById("login");
+loginbtn.addEventListener('click', handlelogin);
+const email=document.getElementById("InputEmail1").value;
+if ${email}==""{
+    alert("Please enter email");
+}
+const pswd=document.getElementById("InputPassword1").value;
